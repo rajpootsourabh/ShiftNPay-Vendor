987462c5d1a2f316bcda00f6040ddc860b5e130b
sujeetbaghel <email4workonpc@gmail.com>
2025-10-05 00:35:02 +0530

    Remove deploy-vender.ps1 from repo

--- deploy-vender.ps1
+++
@@ -1,9 +0,0 @@
-# ------------------------------
-# ShiftNPay Vender Deployment Trigger (local)
-# ------------------------------
-
-$server = "root@45.77.166.189"
-
-Write-Host "Starting remote deployment..."
-ssh $server "bash -l -c '/var/www/vender/deploy.sh'"
-Write-Host "Deployment process finished."
